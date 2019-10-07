820ce934ffc2442fd45f5b55d7f912c67afb64f7
Jakub Nakielski <you@example.com>
2019-10-07 23:54:09 +0200

    Add revealing cards

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,58 @@
 const style = document.createElement("style");
 document.head.appendChild(style);
 
-const cards = document.querySelectorAll(".box__card");
+const cards = [
+  "1.png",
+  "2.png",
+  "3.png",
+  "4.png",
+  "5.png",
+  "6.png",
+  "1.png",
+  "2.png",
+  "3.png",
+  "4.png",
+  "5.png",
+  "6.png"
+];
+const revealCard = el => {
+  let cardId = el.target.id;
+  cardId = cardId.substr(1);
+  console.log(cardId);
 
-const newCards = cards.forEach(el => {
-  el.style.background = `url('./img/ciri.png') `;
-});
+  style.innerHTML = `
+  
+  
+  #${el.target.id} {
+    background: url('./img/${cards[cardId]}');
+    background-size: cover;
+  }
+  
+  `;
+};
+
+const c0 = document.getElementById("c0");
+const c1 = document.getElementById("c1");
+const c2 = document.getElementById("c2");
+const c3 = document.getElementById("c3");
+const c4 = document.getElementById("c4");
+const c5 = document.getElementById("c5");
+const c6 = document.getElementById("c6");
+const c7 = document.getElementById("c7");
+const c8 = document.getElementById("c8");
+const c9 = document.getElementById("c9");
+const c10 = document.getElementById("c10");
+const c11 = document.getElementById("c11");
+
+c0.addEventListener("click", revealCard);
+c1.addEventListener("click", revealCard);
+c2.addEventListener("click", revealCard);
+c3.addEventListener("click", revealCard);
+c4.addEventListener("click", revealCard);
+c5.addEventListener("click", revealCard);
+c6.addEventListener("click", revealCard);
+c7.addEventListener("click", revealCard);
+c8.addEventListener("click", revealCard);
+c9.addEventListener("click", revealCard);
+c10.addEventListener("click", revealCard);
+c11.addEventListener("click", revealCard);
