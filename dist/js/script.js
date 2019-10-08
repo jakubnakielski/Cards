81b5e2efcb2b9204f36db62c745aba9b7da4eb69
Jakub Nakielski <you@example.com>
2019-10-08 20:36:42 +0200

    Add minor fixes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,8 @@
 const style = document.createElement("style");
 document.head.appendChild(style);
 
+const card = new Array();
+
 let turnCounter = 0;
 const turnCounterBox = document.querySelector(".counter__paragraph>i");
 
@@ -36,12 +38,10 @@ function hideCards(first, second) {
 }
 const highlightCard = (first, second, cards, cardId) => {
   second.style.background = `url('./img/${cards[cardId]}')`;
-  console.log(second);
+
   second.classList.add("pair");
   first.classList.add("pair");
   hideCards(first, second);
-  console.log(first);
-  console.log(second);
 };
 
 let oneRevealed;
@@ -81,27 +81,10 @@ const revealCard = el => {
   }
 };
 
-const c1 = document.getElementById("c1");
-const c2 = document.getElementById("c2");
-const c3 = document.getElementById("c3");
-const c4 = document.getElementById("c4"); // MAKE ARRAY
-const c5 = document.getElementById("c5");
-const c6 = document.getElementById("c6");
-const c7 = document.getElementById("c7");
-const c8 = document.getElementById("c8");
-const c9 = document.getElementById("c9");
-const c10 = document.getElementById("c10");
-const c11 = document.getElementById("c11");
+for (i = 0; i <= 11; i++) {
+  card.push(document.getElementById(`c${i}`));
+}
 
-c0.addEventListener("click", revealCard);
-c1.addEventListener("click", revealCard);
-c2.addEventListener("click", revealCard);
-c3.addEventListener("click", revealCard);
-c4.addEventListener("click", revealCard);
-c5.addEventListener("click", revealCard);
-c6.addEventListener("click", revealCard);
-c7.addEventListener("click", revealCard);
-c8.addEventListener("click", revealCard);
-c9.addEventListener("click", revealCard);
-c10.addEventListener("click", revealCard);
-c11.addEventListener("click", revealCard);
+for (i = 0; i <= 11; i++) {
+  card[i].addEventListener("click", revealCard);
+}
